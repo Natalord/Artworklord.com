0cb071e03314297967ed41e8aeaa6d2053f03415
Natalord <onlyforwork643@gmail.com>
2025-03-30 11:51:54 +0530

    Update contactForm.js

--- contactForm.js
+++ contactForm.js
@@ -1,5 +1,1 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    document.getElementById("responseMessage").innerText = "Thank you for your message!";
-    this.reset();
-});
+
